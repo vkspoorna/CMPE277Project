545ef26749ed9a23de78df18f5b9f51dc5335f0c
Son Thai <thaison242@gmail.com>
2016-10-10 00:24:25 -0700

    Update commit

--- app/src/main/java/antitheftproject/android/cmpe277/antitheftproject/RegisterActivity.java
+++ app/src/main/java/antitheftproject/android/cmpe277/antitheftproject/RegisterActivity.java
@@ -36,8 +36,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
